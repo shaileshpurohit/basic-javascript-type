6d1ff663938921eb1879e65458b673d6567a7d02
shaileshpurohit <shaileshpurohit9427@gmail.com>
2026-01-17 09:56:11 +0530

    Refreshing core syntax

---
+++ play.js
@@ -0,0 +1,18 @@
+// Core Syntax
+
+var name = "shailesh";
+var age = 24;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies " +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
